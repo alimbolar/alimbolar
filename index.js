dd94d6448d2e1e7af81410cc9a0170956e3469ec
Alim Bolar <alim@fourplusmedia.com>
2022-05-10 17:32:05 +0800

    added cors middleware

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const mailRouter = require("./routes/mailRouter");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
